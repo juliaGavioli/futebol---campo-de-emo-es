4f864e47ff8513d8c896ea9afca5225513a0d275
julia <julia.gavioli@sptech.school>
2025-05-14 21:36:23 -0300

    Subindo alterações na pagina do quiz

--- Banco de dados/script.sql
+++ Banco de dados/script.sql
@@ -5,16 +5,14 @@ create table usuario (
 idUsuario int primary key auto_increment,
 Nome varchar (45),
 Email varchar (100),
-Senha varchar (45)
-);
+Senha varchar (45));
 
 create table pergunta (
 idPergunta int primary key auto_increment,
 Enunciado varchar (100),
 Alternativa_a varchar (100),
 Alternativa_b varchar (100),
-Alternativa_c varchar (100)
-);
+Alternativa_c varchar (100));
 
 create table resposta (
 idResposta int primary key auto_increment,
@@ -22,11 +20,11 @@ fkusuario int,
 fkpergunta int,
 Resposta char(1),
 RespostaIncorreta char (2),
+Tempogasto time,
 constraint fkusuario
 foreign key (fkusuario) references usuario(idUsuario),
 constraint fkpergunta
-foreign key (fkpergunta) references pergunta(idPergunta)
-);
+foreign key (fkpergunta) references pergunta(idPergunta));
 
 
 
